00f60cc3890ab8e954b654f1c2c3ee839b8a1bed
taewoocode <qkrxodn6035@gmail.com>
2023-11-01 13:05:30 +0900

    main2

---
+++ IdeaProjects/algorithm honza/src/basic/P11659_구간합구하기.java
@@ -0,0 +1,32 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11659_구간합구하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int suNo = Integer.parseInt( stringTokenizer.nextToken());
+        int quizNo = Integer.parseInt( stringTokenizer.nextToken());
+        long[]S = new long[suNo + 1]; //사람은 첫번 째 데이터로 시작, 배열은 0번째로 시작하기에 1을 더함.
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for(int i = 1; i <= suNo; i++) {
+
+            S[i] = S[i - 1] + Integer.parseInt( stringTokenizer.nextToken());
+        }
+
+        for (int q = 0; q<quizNo; q++){
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        }
+
+        int i = Integer.parseInt( stringTokenizer.nextToken());
+        int j = Integer.parseInt( stringTokenizer.nextToken());
+        System.out.println(S[j] - S[i-1]);
+    }
+}
