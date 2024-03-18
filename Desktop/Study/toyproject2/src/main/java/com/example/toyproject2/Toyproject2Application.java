0d3f7a1ab4e0f130b998d4b10585633efb918979
taewoocode <qkrxodn6035@gmail.com>
2024-03-18 11:06:16 +0900

    first commit

---
+++ Desktop/Study/toyproject2/src/main/java/com/example/toyproject2/Toyproject2Application.java
@@ -0,0 +1,13 @@
+package com.example.toyproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toyproject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run( Toyproject2Application.class, args );
+    }
+
+}
